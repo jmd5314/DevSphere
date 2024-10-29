0888ba74f9355ec30bd4662528579a93137b0ece
jmd5314 <jmd5314@gmail.com>
2024-10-29 19:20:20 +0900

    chore: 초기 설정

---
+++ src/main/java/com/devsphere/DevSphereApplication.java
@@ -0,0 +1,13 @@
+package com.devsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsphere/DevSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
